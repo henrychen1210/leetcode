eb0052cb817b69be8035441c1f2b134e4a9aad96
Henry Chen <jenghengchen@gmail.com>
2025-04-11 00:09:17 -0400

    Time: 105 ms (96.94%), Space: 17.8 MB (81.26%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.py
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.py
@@ -6,19 +6,13 @@ class Solution:
         res = 0
 
         for i in range(low, high + 1):
-            
-            if len(str(i)) % 2:
-                continue
-
-            n = int( len( str( i ) ) / 2 )
-
-            head, tail = 0, 0
-
-            for j in range(0, n):
-                head += int(str(i)[j]) 
-                tail += int(str(i)[n + j])
-
-            if head == tail:
+            if i < 100 and i % 11 == 0:
                 res += 1
+                continue
+            if 1000 < i < 10000:
+                head = i // 1000 + i % 1000 // 100
+                tail = i % 10 + i % 100 // 10
+                if head == tail:
+                    res += 1
         
         return res
