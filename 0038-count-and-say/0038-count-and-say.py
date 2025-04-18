000451b46bf70d2b044a66c7c9b082de14a15ed7
Henry Chen <jenghengchen@gmail.com>
2025-04-18 00:31:12 -0400

    Time: 18 ms (5.93%), Space: 18.1 MB (20.58%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,30 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        '''
+        '''
+        if n == 1:
+            return "1"
+        else:
+            return self.helper(self.countAndSay(n - 1))
+
+    def helper(self, s):
+        n = len(s)
+        pair = []
+        pre, count = s[0], 0
+        res = ""
+
+        for i in range(1, n):
+            if pre == s[i]:
+                count += 1
+            else:
+                pair.append([count + 1, int(pre)])
+                pre = s[i]
+                count = 0
+            
+        pair.append([count + 1, int(pre)])
+        print(pair)
+
+        for i in range(len(pair)):
+            res += str(pair[i][0]) + str(pair[i][1])
+
+        return res
