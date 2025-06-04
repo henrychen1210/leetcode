10d617309bc66f6eb3345a3b5922504651d693a8
Henry Chen <jenghengchen@gmail.com>
2025-06-04 17:11:31 -0400

    Time: 19 ms (40.4%), Space: 18 MB (50.33%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.py
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.py
@@ -1,19 +1,9 @@
 class Solution:
-    def lastSubstring(self, s: str) -> str:
-        i, j, n = 0, 1, len(s)
-        while j < n:
-            k = 0
-            while j + k < n and s[i + k] == s[j + k]:
-                k += 1
-            if j + k < n and s[i + k] < s[j + k]:
-                i, j = j, max(j + 1, i + k + 1)
-            else:
-                j = j + k + 1
-        return s[i:]
-
     def answerString(self, word: str, numFriends: int) -> str:
         if numFriends == 1:
             return word
-        last = self.lastSubstring(word)
-        n, m = len(word), len(last)
-        return last[: min(m, n - numFriends + 1)]
+        n = len(word)
+        res = ""
+        for i in range(n):
+            res = max(res, word[i : min(i + n - numFriends + 1, n)])
+        return res
