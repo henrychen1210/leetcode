92f3348e729dd3e6455c4b0e1bca118ff1a9768e
Henry Chen <jenghengchen@gmail.com>
2025-12-18 16:00:44 -0800

    Time: 251 ms (82.87%), Space: 33.7 MB (90.34%) - LeetHub

--- 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.py
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.py
@@ -1,18 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
-        '''
-        '''
         n = len(prices)
-        profitSum = [0] * (n + 1)
         priceSum = [0] * (n + 1)
+        profitSum = [0] * (n + 1)
+
         for i in range(n):
-            profitSum[i + 1] = profitSum[i] + prices[i] * strategy[i]
             priceSum[i + 1] = priceSum[i] + prices[i]
-        res = profitSum[n]
-        for i in range(k - 1, n):
-            leftProfit = profitSum[i - k + 1]
-            rightProfit = profitSum[n] - profitSum[i + 1]
-            changeProfit = priceSum[i + 1] - priceSum[i - k // 2 + 1]
-            res = max(res, leftProfit + changeProfit + rightProfit)
-        return res
+            profitSum[i + 1] = profitSum[i] + prices[i] * strategy[i]
         
+        res = profitSum[n]
+        
+        for i in range(k - 1, n):
+            left = profitSum[i - k + 1]
+            right = profitSum[n] - profitSum[i + 1]
+            change = priceSum[i + 1] - priceSum[i - k // 2 + 1]
+            res = max(res, left + right + change)
+        return res 
