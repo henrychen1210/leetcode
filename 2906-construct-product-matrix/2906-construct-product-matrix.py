bf04c1b2e62059972afa5325be7d689f22dfd6f2
Henry Chen <jenghengchen@gmail.com>
2026-03-24 14:05:24 -0700

    Time: 103 ms (86.13%), Space: 43 MB (66.47%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.py
+++ 2906-construct-product-matrix/2906-construct-product-matrix.py
@@ -3,33 +3,18 @@ class Solution:
         mod = 12345
         m = len(grid)
         n = len(grid[0])
-        prefix = [[0] * n for _ in range(m)]
-        sufix = [[0] * n for _ in range(m)]
         p = [[0] * n for _ in range(m)]
 
-        # Build prefix (product of all elements before [i][j] in row-major order)
+        prefix = 1
         for i in range(m):
             for j in range(n):
-                if i == 0 and j == 0:
-                    prefix[i][j] = 1
-                elif j == 0:
-                    prefix[i][j] = prefix[i-1][n-1] * grid[i-1][n-1] % mod
-                else:
-                    prefix[i][j] = prefix[i][j-1] * grid[i][j-1] % mod
+                p[i][j] = prefix
+                prefix = prefix * grid[i][j] % mod
 
-        # Build suffix (product of all elements after [i][j] in row-major order)
+        sufix = 1
         for i in range(m-1, -1, -1):
             for j in range(n-1, -1, -1):
-                if i == m-1 and j == n-1:
-                    sufix[i][j] = 1
-                elif j == n-1:
-                    sufix[i][j] = sufix[i+1][0] * grid[i+1][0] % mod
-                else:
-                    sufix[i][j] = sufix[i][j+1] * grid[i][j+1] % mod
-
-        # Combine
-        for i in range(m):
-            for j in range(n):
-                p[i][j] = prefix[i][j] * sufix[i][j] % mod
+                p[i][j] = (p[i][j] * sufix) % mod
+                sufix = sufix * grid[i][j] % mod
 
         return p
