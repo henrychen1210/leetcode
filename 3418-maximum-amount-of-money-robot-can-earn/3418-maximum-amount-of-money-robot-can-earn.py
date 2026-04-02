b54312828cf08193e5ea6b259d82db76f407a02b
Henry Chen <jenghengchen@gmail.com>
2026-04-02 15:14:48 -0700

    Time: 1591 ms (87.85%), Space: 45.7 MB (94.39%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.py
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.py
@@ -30,45 +30,45 @@ class Solution:
         m = len(coins)
         n = len(coins[0])
 
-        dp = [[[float('-inf')] * 3 for _ in range(n)] for _ in range(m)]
+        dp = [[float('-inf')] * 3 for _ in range(n)]
 
-        dp[0][0][0] = coins[0][0]
+        dp[0][0] = coins[0][0]
 
         for k in range(1, 3):
-            dp[0][0][k] = max(coins[0][0], 0)
-
-        for i in range(1, m):
-            dp[i][0][0] = dp[i - 1][0][0] + coins[i][0]
-            for k in range(1, 3):
-                dp[i][0][k] = max(
-                                dp[i - 1][0][k] + coins[i][0],
-                                dp[i - 1][0][k - 1]
-                            )
+            dp[0][k] = max(coins[0][0], 0)
 
         for j in range(1, n):
-            dp[0][j][0] = dp[0][j - 1][0] + coins[0][j]
-            x = max(coins[0][j], 0)
+            dp[j][0] = dp[j - 1][0] + coins[0][j]
             for k in range(1, 3):
-                dp[0][j][k] = max(
-                                dp[0][j - 1][k] + coins[0][j],
-                                dp[0][j - 1][k - 1]
-                            )
+                dp[j][k] = max(
+                    dp[j - 1][k] + coins[0][j],
+                    dp[j - 1][k - 1]
+                )
 
         for i in range(1, m):
+            curr = [[float("-inf")] * 3 for _ in range(n)]
+            curr[0][0] = dp[0][0] + coins[i][0]
+
+            for k in range(1, 3):
+                curr[0][k] = max(
+                    dp[0][k] + coins[i][0],
+                    dp[0][k - 1]
+                )
+
+
             for j in range(1, n):
-                dp[i][j][0] = max(
-                                    dp[i - 1][j][0] + coins[i][j],
-                                    dp[i][j - 1][0] + coins[i][j],
-                                )
+                curr[j][0] = max(
+                    curr[j - 1][0] + coins[i][j],
+                    dp[j][0] + coins[i][j],
+                )
 
                 for k in range(1, 3):
-                    dp[i][j][k] = max(
-                                    dp[i - 1][j][k] + coins[i][j],
-                                    dp[i][j - 1][k] + coins[i][j],
-                                    dp[i - 1][j][k - 1],
-                                    dp[i][j - 1][k - 1]
-                                )
+                    curr[j][k] = max(
+                        curr[j - 1][k] + coins[i][j],
+                        dp[j][k] + coins[i][j],
+                        curr[j - 1][k - 1],
+                        dp[j][k - 1]
+                    )
+            dp = curr
 
-        return dp[m - 1][n - 1][2]
-
-
+        return dp[n - 1][2]
