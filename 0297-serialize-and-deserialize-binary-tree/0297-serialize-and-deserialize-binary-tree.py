80c21118f0b6b6201ce71c88ce520cc4b12fc5de
Henry Chen <jenghengchen@gmail.com>
2026-04-07 16:04:06 -0700

    Time: 85 ms (77.21%), Space: 22.7 MB (38.36%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque 
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root: return "None"
+        res, q = [], deque([root])
+        while q:
+            node = q.popleft()
+            if node:
+                res.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                res.append("None")
+        return ','.join(res)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == "None": return None
+        nodes = data.split(",")
+        root = TreeNode(int(nodes[0]))
+        q = deque([root])
+        i = 1
+        while q:
+            curr = q.popleft()
+            if nodes[i] != "None":
+                curr.left = TreeNode(int(nodes[i]))
+                q.append(curr.left)
+            i += 1
+            if i < len(nodes) and nodes[i] != "None":
+                curr.right = TreeNode(int(nodes[i]))
+                q.append(curr.right)
+            i += 1
+        return root
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
