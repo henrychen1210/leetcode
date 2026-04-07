436626f8776e176b07c4ff434c5f5b2b6d8299eb
Henry Chen <jenghengchen@gmail.com>
2026-04-07 16:41:51 -0700

    Time: 4 ms (63.3%), Space: 21.4 MB (53.04%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,25 +7,30 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         '''
-        preorder = [3, 9, 20, 15, 7] root -> left -> right
-        inorder = [9, 3, 15, 20, 7] left -> root -> right
+         preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
 
-        mid = 1 (3)
+         mid = 1 (3)
+
+        1: mid
+        mid + 1: 
+
+        : mid
+
+        mid + 1:
         '''
+        inorder_map = { value: index for index, value in enumerate(inorder)}
 
-        if not preorder or not inorder:
-            return None
+        def helper(pre_start, pre_end, in_start, in_end):
+            if pre_start > pre_end:
+                return None
+            
+            root = TreeNode(preorder[pre_start])
+            mid = inorder_map[preorder[pre_start]]
+            left_size = mid - in_start
 
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
+            root.left = helper(pre_start + 1, pre_start + left_size, in_start, mid - 1)
+            root.right = helper(pre_start + left_size + 1, pre_end, mid + 1, in_end)
 
-        left_preorder = preorder[1: mid + 1]
-        right_preorder = preorder[mid + 1:]
-        left_inorder = inorder[: mid]
-        right_inorder = inorder[mid  + 1:]
-
-        root.left = self.buildTree(left_preorder, left_inorder)
-        root.right = self.buildTree(right_preorder, right_inorder)
-
-        return root
-
+            return root
+        
+        return helper(0, len(preorder) - 1, 0, len(inorder) - 1)
