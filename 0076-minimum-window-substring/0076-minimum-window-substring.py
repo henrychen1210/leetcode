96d57ec65b5aee70ed5bd417efba4f4db80fb374
Henry Chen <jenghengchen@gmail.com>
2026-04-06 17:18:47 -0700

    Time: 399 ms (11.8%), Space: 19.6 MB (82.41%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,7 +1,14 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        freq = [0] * 52          # 0-25: lowercase, 26-51: uppercase
-        window_freq = [0] * 52
+        '''
+        Input: s = "ADOBECODEBANC", t = "ABC"
+        freq = {A: 1, B: 1, C: 1}
+
+        win_freq = {A: 1}
+        [A, B]
+        '''
+        need = [0] * 52
+        window = [0] * 52
 
         def char_index(c):
             if c.islower():
@@ -16,18 +23,21 @@ class Solution:
             return True
 
         for c in t:
-            freq[char_index(c)] += 1
+            need[char_index(c)] += 1
 
+        
         left = 0
         res = ""
 
         for right in range(len(s)):
-            window_freq[char_index(s[right])] += 1
-
-            while helper(window_freq, freq):
-                if res == "" or right - left + 1 < len(res):
-                    res = s[left:right + 1]
-                window_freq[char_index(s[left])] -= 1
+            window[char_index(s[right])] += 1
+            
+            while helper(window, need):
+                if res == "" or len(res) > right - left + 1:
+                    res = s[left: right + 1]
+                window[char_index(s[left])] -= 1
                 left += 1
 
         return res
+
+
