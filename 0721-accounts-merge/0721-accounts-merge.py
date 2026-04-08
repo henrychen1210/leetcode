407e2659cc6a016103daab6d3f6294569e0ef06d
Henry Chen <jenghengchen@gmail.com>
2026-04-08 14:54:56 -0700

    Time: 23 ms (80.29%), Space: 22.1 MB (91.93%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,33 @@
+class UF:
+    def __init__(self, N):
+        self.parents = list(range(N))
+    def union(self, child, parent):
+        self.parents[self.find(child)] = self.find(parent)
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        '''
+        [name, emails ...]
+
+        return [name, emails]
+        '''
+        uf = UF(len(accounts))
+        ownership = {}
+        for i, (_, *emails) in enumerate(accounts):
+            for email in emails:
+                if email in ownership:
+                    uf.union(i, ownership[email])
+                ownership[email] = i
+        
+        ans = collections.defaultdict(list)
+        for email, owner in ownership.items():
+            ans[uf.find(owner)].append(email)
+        
+        print(ans)
+        
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
+
