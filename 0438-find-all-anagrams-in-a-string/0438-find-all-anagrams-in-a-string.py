06fb4888921e8f40a4a14e1808dd867b56c8b8a8
Henry Chen <jenghengchen@gmail.com>
2026-04-09 16:38:59 -0700

    Time: 35 ms (74.49%), Space: 19.8 MB (51.81%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,50 +1,30 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        '''
-        s = "cbaebabacd", p = "abc"
-        freq = { a: 1, b: 1, c:1}
-
-        size = 3, {a: 1, b: 1, c: 1}
-        left
-        0, {a: 1, b: 1, c: 1} V res = [0]
-        1, {a: 1, b: 1, e: 1} X
-
-
-        '''
-
+        
         p_freq = {}
         win_freq = {}
         size = len(p)
-        left = 0
-        res = []
 
         for c in p:
             p_freq[c] = p_freq.get(c, 0) + 1
-
+        
         need = len(p_freq)
         have = 0
+        left = 0
+        res = []
 
-        for c in s[:size]:
-            win_freq[c] = win_freq.get(c, 0) + 1
-            if win_freq[c] == p_freq.get(c, 0):
-                have += 1
-
-        if need == have:
-            res.append(left)
-        
-        while left + size < len(s):
-            c = s[left]
-            if win_freq[c] == p_freq.get(c, 0):
-                have -= 1
-            win_freq[c] -= 1
-
-            left += 1
-            c = s[left + size - 1]
+        for i, c in enumerate(s):
             win_freq[c] = win_freq.get(c, 0) + 1
             if win_freq[c] == p_freq.get(c, 0):
                 have += 1
             
-            if need == have:
+            if i - left + 1 > size:
+                if win_freq[s[left]] == p_freq.get(s[left], 0):
+                    have -= 1
+                win_freq[s[left]] -= 1
+                left += 1
+            
+            if have == need:
                 res.append(left)
         
         return res
