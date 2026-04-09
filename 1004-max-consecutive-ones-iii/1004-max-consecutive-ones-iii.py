5cd9a3b43b44880591aee3fe6ba0c3fecce631da
Henry Chen <jenghengchen@gmail.com>
2026-04-09 15:09:23 -0700

    Time: 66 ms (30.48%), Space: 21.9 MB (98.64%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,38 +1,36 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         '''
+        left, right = 0, 0
+        zero = 0
         nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
-        res = max(res, len(wondow)) 
+        
+        res = max(res, right - left = 1)
 
-            zero
-        [1] 0
-        [1,1] 0
-        [1,1,1] 0
-        [1,1,1,0] 1
-        [1,1,1,0,0] 2
-        [1,1,1,0,0,0] 3 X -> [1,1,0,0,0] 3 x -> ... -> [0,0] 2
-        [0,0,1] 2
-        []  
-
-        Time O(n)
-        Space O(1)
-
+        left, rihgt, zero
+        0, 0, 0 [1]
+        0, 1, 0 [1, 1]
+        ;
+        ;
+        0, 3, 1 [1,1,1,0]
+        0, 4, 2 [1,1,1,0,0]
+        0, 5, 3 [1,1,1,0,0,0] X -> 1, 5, 3 [1,1,0,0,0] -> 4, 5, 2, [0,0] 
         '''
 
         left = 0
         zero = 0
         res = 0
-        n = len(nums)
 
-        for right in range(n):
+        for right in range(len(nums)):
             if nums[right] == 0:
                 zero += 1
-
-            while zero > k:
+            
+            while zero > k and left <= right:
                 if nums[left] == 0:
                     zero -= 1
                 left += 1
-            
-            res = max(res, right - left + 1)
+
+            if zero <= k:
+                res = max(res, right - left + 1)
         
         return res
