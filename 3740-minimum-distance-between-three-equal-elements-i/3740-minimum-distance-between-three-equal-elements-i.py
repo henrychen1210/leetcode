02608f0619b8ed3de249b18f835ca3f7e5fe0a28
Henry Chen <jenghengchen@gmail.com>
2026-04-10 10:10:36 -0700

    Time: 0 ms (100%), Space: 19.3 MB (48.66%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
@@ -1,25 +1,17 @@
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
-        '''
-        nums = [1,1,2,3,2,1,2]
-
-        res = min(res, x)
-        i, j, k
-        0, 1, 5 =  1 + 5 + 5 = 11
-        '''
-
+        last = {}  # val -> [prev_index, last_index]
         res = float("inf")
-        for i in range(len(nums)):
-            j = i + 1
-            while j < len(nums) and nums[j] != nums[i]:
-                j += 1
-            
-            k = j + 1
-            while k < len(nums) and nums[k] != nums[i]:
-                k += 1
-            
-            if j < len(nums) and k < len(nums):
-                x = abs(i - j) + abs(j - k) + abs(k - i)
-                res = min(res, x)
-        
+
+        for k, c in enumerate(nums):
+            if c in last:
+                j = last[c][-1]
+                if len(last[c]) > 1:
+                    i = last[c][-2]
+                    x = abs(i - j) + abs(j - k) + abs(k - i)
+                    res = min(res, x)
+                last[c].append(k)
+            else:
+                last[c] = [k]
+
         return res if res != float("inf") else -1
