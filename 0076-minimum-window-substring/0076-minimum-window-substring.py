c8203259d0108a1cc5cf3b8d46fae6392b484eb5
Henry Chen <jenghengchen@gmail.com>
2026-04-14 16:17:20 -0700

    Time: 123 ms (33.49%), Space: 19.7 MB (79.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,29 +1,41 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        t_freq = {}
-        window_freq = {}
+        '''
+        freq = {}
+        target_freq = {}
+        need, have = 0, 0
+
+
+
+        '''
+
+        if len(s) < len(t):
+            return ""
+
+        target_freq = {}
+        freq = {}
 
         for c in t:
-            t_freq[c] = t_freq.get(c, 0) + 1
+            target_freq[c] = target_freq.get(c, 0) + 1
+        
+        need, have = len(target_freq), 0
 
-        need = len(t_freq)
-        have = 0
         left = 0
         res = ""
 
         for right, c in enumerate(s):
-            window_freq[c] = window_freq.get(c, 0) + 1
-            if window_freq[c] == t_freq.get(c, 0):
+            freq[c] = freq.get(c, 0) + 1
+            if c in target_freq and freq[c] == target_freq[c]:
                 have += 1
             
-            while have == need:
+            while left <= right and have == need:
                 if res == "" or len(res) > right - left + 1:
-                    res = s[left: right + 1]
+                    res = s[left:right + 1]
                 
-                d = s[left]
-                if window_freq[d] == t_freq.get(d, 0):
+                left_c = s[left]
+                if left_c in target_freq and freq[left_c] == target_freq[left_c]:
                     have -= 1
-                window_freq[d] -= 1
+                freq[left_c] -= 1
                 left += 1
-            
-        return res
+        
+        return res 
